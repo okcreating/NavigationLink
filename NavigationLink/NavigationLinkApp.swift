a7ed9159c5e07b03bf8c4f01d96a1225f54ca2af
okcreating <oksibearus@gmail.com>
2024-12-25 13:41:17 +0000

    Initial Commit

---
+++ NavigationLink/NavigationLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationLinkApp.swift
+//  NavigationLink
+//
+//  Created by Oksana Kazarinova on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
